b3a35b88c6d28d6ed4c85c6b3e1212708138679c
Mario Fink <knif.oiram@gmail.com>
2020-11-19 17:17:12 +0100

    go deviceagent: complete set of RPCs

--- lib/go/deviceagent/main.go
+++ lib/go/deviceagent/main.go
@@ -60,10 +60,132 @@ func main() {
   }
   defer clnt.Close()
 
+	// use device serial number as RPC identifier
+	clientid := "813e9e53-fe1f-4a27-a1bc-a97e8846a5a2"
+
 	// start registering procedures...
 
-  err = clnt.Register("testfunc", TestFunction, nil)
-  if err != nil {
+  // APPS
+	if clnt.Register("re.mgmt." + clientid + ".is_running", IsRunning, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".write_data", WriteData, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_remove_image", DockerRemoveImage, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_tag", DockerTag, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_remove_container", DockerRemoveContainer, nil) != nil {
+    panic(err)
+  }
+
+  // CONFIG
+	if clnt.Register("re.mgmt." + clientid + ".readme", Readme, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".updater", Updater, nil) != nil {
+    panic(err)
+  }
+
+  // DEVICE START, STOP, UPDATE
+  if clnt.Register("re.mgmt." + clientid + ".agent_update", AgentUpdate, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".system_reboot", SystemReboot, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".agent_restart", AgentRestart, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".device_handshake", DeviceHandshake, nil) != nil {
+    panic(err)
+  }
+
+  // FIREWALL
+  if clnt.Register("re.mgmt." + clientid + ".apply_firewall", ApplyFirewall, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".ufw_enable", UfwEnable, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".ufw_status", UfwStatus, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".ufw_allow", UfwAllow, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".ufw_reset", UfwReset, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".ufw_listening", UfwListening, nil) != nil {
+    panic(err)
+  }
+
+  // WIFI
+  if clnt.Register("re.mgmt." + clientid + ".get_wifi", GetWifi, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".add_wifi", AddWifi, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".scan_wifi", ScanWifi, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".remove_wifi", RemoveWifi, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".restart_wifi", RestartWifi, nil) != nil {
+    panic(err)
+  }
+
+  // DOCKER STATS
+  if clnt.Register("re.mgmt." + clientid + ".docker_ps", DockerPs, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_logs", DockerLogs, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_stats", DockerStats, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + "docker_images", DockerImages, nil) != nil {
+    panic(err)
+  }
+
+  // DOCKER LIFECYCLE
+  if clnt.Register("re.mgmt." + clientid + ".docker_pull", DockerPull, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_run", DockerRun, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_push", DockerPush, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_build", DockerBuild, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_build_cancel", DockerBuildCancel, nil) != nil {
+    panic(err)
+  }
+
+  // DOCKER PRUNE
+  if clnt.Register("re.mgmt." + clientid + ".docker_prune_all", DockerPruneAll, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_prune_images", DockerPruneImages, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_prune_volumes", DockerPruneVolumes, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_prune_networks", DockerPruneNetworks, nil) != nil {
+    panic(err)
+  }
+  if clnt.Register("re.mgmt." + clientid + ".docker_prune_containers", DockerPruneContainers, nil) != nil {
     panic(err)
   }
 
--- lib/go/deviceagent/procedures.go
+++ lib/go/deviceagent/procedures.go
@@ -8,21 +8,128 @@ import (
   "github.com/gammazero/nexus/v3/wamp"
 )
 
-func TestFunction(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
-  x, _ := wamp.AsInt64(inv.Arguments[0])
-  y, _ := wamp.AsInt64(inv.Arguments[1])
-  z := x + y
-  return client.InvokeResult{Args: wamp.List{z}}
-}
+// func TestFunction(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+//   x, _ := wamp.AsInt64(inv.Arguments[0])
+//   y, _ := wamp.AsInt64(inv.Arguments[1])
+//   z := x + y
+//   return client.InvokeResult{Args: wamp.List{z}}
+// }
 
-func sum(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
-    var sum int64
-    for _, arg := range inv.Arguments {
-        n, _ := wamp.AsInt64(arg)
-        sum += n
-    }
-    return client.InvokeResult{Args: wamp.List{sum}}
+// APPS
+func IsRunning(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
 }
-
+func WriteData(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerRemoveImage(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerTag(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerRemoveContainer(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+// CONFIG
+func Readme(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func Updater(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+// DEVICE START, STOP, UPDATE
+func AgentUpdate(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func SystemReboot(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func AgentRestart(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DeviceHandshake(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+// FIREWALL
+func ApplyFirewall(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func UfwEnable(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func UfwStatus(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func UfwAllow(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func UfwReset(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func UfwListening(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+// WIFI
+func GetWifi(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func AddWifi(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func ScanWifi(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func RemoveWifi(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func RestartWifi(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+// DOCKER STATS
+func DockerPs(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerLogs(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerStats(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerImages(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+// DOCKER LIFECYCLE
+func DockerPull(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerRun(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerPush(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerBuild(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerBuildCancel(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+// DOCKER PRUNE
+func DockerPruneAll(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerPruneImages(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerPruneVolumes(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerPruneNetworks(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
+func DockerPruneContainers(ctx context.Context, inv *wamp.Invocation) client.InvokeResult {
+  return client.InvokeResult{Args: wamp.List{}}
+}
 
 // ------------------------------------------------------------------------- //
