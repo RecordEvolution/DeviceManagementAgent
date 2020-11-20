0f943e0d8d3d76e11bc1c952e7ae4b6538cfda52
Mario Fink <knif.oiram@gmail.com>
2020-11-20 16:37:59 +0100

    monitor stats of certain process

--- shl/monitor-stats.sh
+++ shl/monitor-stats.sh
@@ -1,16 +1,94 @@
 #!/bin/bash
 
-fl="$1"
+# --------------------------------------------------------------------------- #
+
+helpmessage=$(cat << 'EOF'
+  ...
+  Usage:  $ ./monitor-stats.sh --file=<file/path> --timestep=<#seconds> --intervals=<#intervals/steps> --process=<command>
+  ...
+EOF
+)
+
+# number of args
+#echo "$#"
+#
+# echo "arg1 $1"
+# echo "arg2 $2"
+# echo "arg3 $3"
+# echo "arg4 $4"
+
+# concatetenate all arguments
+cliargs="$1 $2 $3 $4"
+#echo "${cliargs}"
+
+if [[ "$1" == "--help" ]]; then
+  echo -e "${helpmessage}"
+  exit 0
+fi
+
+# extract file/path
+fl=$(echo "${cliargs}" | grep "\-\-file=[^ ]*" -o | awk -F '=' '{print $2}')
+
 if [ -z "${fl}" ]; then
-  echo "please provide a file/path to record the stats" >&2
+  echo -e "${helpmessage}"
   exit 1
 fi
+
+# extract timestep or set default
+dt=$(echo "${cliargs}" | grep "\-\-timestep=[^ ]*" -o | awk -F '=' '{print $2}')
+
+if [ -z "${dt}" ]; then
+  dt=1
+fi
+
+# extract number of intervals or set default
+N=$(echo "${cliargs}" | grep "\-\-intervals=[^ ]*" -o | awk -F '=' '{print $2}')
+
+if [ -z "${N}" ]; then
+  N=300
+fi
+
+# extract command name
+pr=$(echo "${cliargs}" | grep "\-\-process=[^ ]*" -o | awk -F '=' '{print $2}')
+
+if [ -z "${pr}" ]; then
+  echo -e "${helpmessage}"
+  exit 1
+fi
+
+echo "file:      ${fl}"
+echo "timestep:  ${dt}"
+echo "intervals: ${N}"
+echo "process:   ${pr}"
+
+# --------------------------------------------------------------------------- #
+
+echo "#Timestamp,Time,MemTotal[kB],MemFree[kB],MemAvailable[kB],VSZ(${pr})[kB],RSS(${pr})[kB]" > "${fl}"
 
 # ask for selection of current stats, i.a. memory, cpu load, ...
 query_stats()
 {
+  ts=$(date +%Y-%m-%d_%H-%M-%S) #-%N)
+  tm=$(date +%H:%M:%S)
 
+  memtota=$(cat /proc/meminfo | grep "MemTotal" | awk -F ':' '{print $2}' | awk '{print $1}' | tr -d ' \n')
+  memfree=$(cat /proc/meminfo | grep "MemFree" | awk -F ':' '{print $2}' | awk '{print $1}' | tr -d ' \n')
+  memavai=$(cat /proc/meminfo | grep "MemAvailable" | awk -F ':' '{print $2}' | awk '{print $1}' | tr -d ' \n')
 
+  # get stats of certain process
+  prstats=$(ps -o comm,vsz,rss,args | grep "${pr}" | grep -v "grep" | grep -v "$0" | head -n1)
+  prvsz=$(echo "${prstats}" | awk '{print $2}')
+  if [[ ! -z "$(echo "${prvsz}" | grep m)" ]]; then
+	  prvsz=$(echo "${prvsz}" | tr -d "m")
+	  prvsz=$((prvsz*1000))
+  fi
+  prrss=$(echo "${prstats}" | awk '{print $3}')
+  if [[ ! -z "$(echo "${prrss}" | grep m)" ]]; then
+	  prrss=$(echo "${prrss}" | tr -d "m")
+	  prrss=$((prrss*1000))
+  fi
+
+  echo "${ts},${tm},${memtota},${memfree},${memavai},${prvsz},${prrss}"
 }
 
 # BusyBox v1.32.0 (2020-11-10 12:33:27 UTC) multi-call binary.
@@ -49,10 +127,9 @@ query_stats()
 #                      sid,stat,
 #                      rss (Resident Set Size)
 
-# set temporal resolutiong for stat monitoring (in seconds)
-dt=2
-# number of intervals to monitor
-N=200
+
+# --------------------------------------------------------------------------- #
+
 # count intervals
 cnt=0
 
@@ -62,7 +139,8 @@ do
   echo "monitoring stats... interval ${cnt}"
 
   # query stats
-  # query_stats()
+  data=$(query_stats)
+  echo "${data}" >> "${fl}"
 
   # iterate interval counter
   cnt=$((cnt+1))
@@ -71,3 +149,6 @@ do
   sleep ${dt}
 
 done
+
+
+# --------------------------------------------------------------------------- #
