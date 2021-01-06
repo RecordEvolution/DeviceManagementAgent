4f73147f5d7d1c182ca840e80e4c40ea5a28a4f9
Mario Fink <knif.oiram@gmail.com>
2021-01-06 18:20:34 +0100

    wampcc: test setup

--- lib/cpp/wampcc/client.cpp
+++ lib/cpp/wampcc/client.cpp
@@ -13,33 +13,67 @@ int main(int argc, char* argv[])
   conf.ssl.enable = true;
   wampcc::kernel the_kernel(conf, wampcc::logger::console());
 
+  /* Create the TCP socket and attempt to connect. */
+  std::unique_ptr<wampcc::tcp_socket> socket(new wampcc::tcp_socket(&the_kernel));
+  socket->connect("127.0.0.1", 55555).wait_for(std::chrono::seconds(3));
+
   /* Create the SSL socket, in connector mode. */
-  wampcc::ssl_socket socket(&the_kernel);
+  // wampcc::ssl_socket socket(&the_kernel);
 
   // try to connect
   // socket.connect("cb.reswarm.io",8080).wait_for(std::chrono::seconds(3));
-  socket.connect("127.0.0.1",80).wait_for(std::chrono::seconds(3));
-  if (!socket.is_connected())
+  // socket->connect("127.0.0.1", 55555).wait_for(std::chrono::seconds(3));
+
+  if (!socket->is_connected())
     throw std::runtime_error("connect failed");
 
-  /* Create the wampcc kernel. */
-  // wampcc::kernel the_kernel;
-
-  /* Create the TCP socket and attempt to connect. */
-  // std::unique_ptr<wampcc::tcp_socket> socket(new wampcc::tcp_socket(&the_kernel));
-  // socket->connect("cb.reswarm.io",8080).wait_for(std::chrono::seconds(3));
-  //
-  // if (!socket->is_connected())
-  //   throw std::runtime_error("connect failed");
-
   /* With the connected socket, create a wamp session & logon to the realm
-   * * called 'default_realm'. */
- // auto session = wampcc::wamp_session::create<wampcc::websocket_protocol>(&the_kernel, std::move(socket));
- //
- // session->hello("realm1").wait_for(std::chrono::seconds(3));
- //
- //  if (!session->is_open())
- //    throw std::runtime_error("realm logon failed");
+   * called 'default_realm'. */
+  auto session = wampcc::wamp_session::create<wampcc::websocket_protocol>(&the_kernel,
+                                                                          std::move(socket));
+
+  session->hello("default_realm").wait_for(std::chrono::seconds(3));
+
+  if (!session->is_open())
+    throw std::runtime_error("realm logon failed");
+
+  /* Call a remote procedure. */
+  session->call("greeting", {}, {},
+    [](wampcc::wamp_session&, wampcc::result_info result) {
+      if (result)
+        std::cout << "got result: " << result.args.args_list[0] << std::endl;
+      }
+  );
+
+  // session->call("math.service.add", {}, {{100, 200}, {}},
+  //                   [](wampcc::wamp_session&, wampcc::result_info result) {
+  //       if (result)
+  //         std::cout << "got result: " << result.args.args_list[0] << std::endl;
+  //     });
+
+  /* Register a procedure that can sum an array of numbers. */
+  session->provide("math.service.add", {},
+    [](wampcc::wamp_session&, wampcc::registered_info info) {
+      if (info)
+        std::cout << "procedure registered with id "
+                  << info.registration_id << std::endl;
+      else
+        std::cout << "procedure registration failed, error "
+                  << info.error_uri << std::endl;
+    },
+    [](wampcc::wamp_session& ws, wampcc::invocation_info info) {
+      int total = 0;
+      for (auto& item : info.args.args_list)
+        if (item.is_int())
+          total += item.as_int();
+      ws.yield(info.request_id, {total});
+    }
+  );
+
+  session->closed_future().wait_for(std::chrono::seconds(10));
+  session->close().wait();
+
+  std::cout<<"finishing (C++)-WAMP client...\n";
 
   return 0;
 }
--- lib/cpp/wampcc/router.cpp
+++ lib/cpp/wampcc/router.cpp
@@ -24,9 +24,19 @@ int main(int argc, char* argv[])
 
     /* Provide an RPC named 'greeting' on realm 'default_realm'. */
     router.callable("default_realm", "greeting",
-                    [](wampcc::wamp_router&, wampcc::wamp_session& caller, wampcc::call_info info) {
-      caller.result(info.request_id, {"hello"});
-    });
+      [](wampcc::wamp_router&, wampcc::wamp_session& caller, wampcc::call_info info) {
+        std::cout<<"default_realm.greeting:"<<info.args.args_list<<"\n";
+        caller.result(info.request_id, {"hello"});
+      }
+    );
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+
+    std::vector<wampcc::socket_address> listadd = router.get_listen_addresses();
+    for ( auto add: listadd )
+      std::cout<<"router listening on "<<add.to_string()<<"\n";
+
+    std::cout<<"done for (C++)-WAMP router...\n";
 
     /* Suspend main thread */
     std::promise<void> forever;
@@ -36,7 +46,7 @@ int main(int argc, char* argv[])
 
     std::cout << e.what() << "\n";
     return 1;
-    
+
   }
 
   return 0;
