4646b32e0d426acc66871276a2d5f00565fa9fe0
DewitteRuben <ruben.dewitte@proximus.be>
2021-01-21 17:38:49 +0100

    feat: more docker methods, docker only impl for now

--- src/container/container.go
+++ src/container/container.go
@@ -10,6 +10,8 @@ import (
 	"sync"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
 )
 
@@ -34,27 +36,29 @@ const (
 
 const defaultRegistry = "https://index.docker.io/v1/"
 
-var containerInstance Container
-var containerType = DOCKER
+// For now stick only with Docker as implementation
+var containerInstance *Docker
+
+// var containerType = DOCKER
 var instanceLock sync.Once
 
 // GetClientInstance creates or gets an instance of the set container API. (Docker by default)
-func GetClientInstance() (Container, error) {
+func GetClientInstance() (*Docker, error) {
 	var initalizationError error
 	instanceLock.Do(func() {
-		switch containerType {
-		case DOCKER:
-			client, err := newDockerClient()
-			if err != nil {
-				initalizationError = err
-			} else {
-				containerInstance = &Docker{client: client}
-			}
-		case LXC:
-			{
-				initalizationError = fmt.Errorf("Not yet implemented")
-			}
+		// switch containerType {
+		// case DOCKER:
+		client, err := newDockerClient()
+		if err != nil {
+			initalizationError = err
+		} else {
+			containerInstance = &Docker{client: client}
 		}
+		// case LXC:
+		// 	{
+		// 		initalizationError = fmt.Errorf("Not yet implemented")
+		// 	}
+		// }
 	})
 
 	if initalizationError != nil {
@@ -64,17 +68,17 @@ func GetClientInstance() (Container, error) {
 	return containerInstance, nil
 }
 
-// SetContainerAPI sets the which api should be used (Docker, LXC). Docker by default.
-// Has to be called before GetClientInstance
-func SetContainerAPI(apiType Type) {
-	containerType = apiType
-}
+// // SetContainerAPI sets the which api should be used (Docker, LXC). Docker by default.
+// // Has to be called before GetClientInstance
+// func SetContainerAPI(apiType Type) {
+// 	containerType = apiType
+// }
 
 func newDockerClient() (*client.Client, error) {
 	return client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 }
 
-// ListImages lists all images available on the current device. Marshal type for Docker: types.ImageListOptions
+// ListImages lists all images available on the host.
 func (docker *Docker) ListImages(ctx context.Context, options interface{}) (string, error) {
 	var rOptions types.ImageListOptions
 	if options == nil {
@@ -124,34 +128,66 @@ func (docker *Docker) Login(ctx context.Context, username string, password strin
 	return authOkBody.Status, nil
 }
 
-// Pull pulls a docker image from a registry
+// Pull pulls a container image from a registry
 func (docker *Docker) Pull(ctx context.Context, imageName string) (io.ReadCloser, error) {
-	closableReader, err := docker.client.ImagePull(ctx, imageName, types.ImagePullOptions{})
+	return docker.client.ImagePull(ctx, imageName, types.ImagePullOptions{})
+}
 
+// Push pushes a container image to a registry
+func (docker *Docker) Push(ctx context.Context, imageName string) (io.ReadCloser, error) {
+	return docker.client.ImagePush(ctx, imageName, types.ImagePushOptions{})
+}
+
+// Stats gets the stats of a specific container
+func (docker *Docker) Stats(ctx context.Context, containerID string) (io.ReadCloser, error) {
+	stats, err := docker.client.ContainerStats(ctx, containerID, true)
 	if err != nil {
 		return nil, err
 	}
+	return stats.Body, nil
+}
 
-	return closableReader, nil
+// Run creates and starts a specific container
+func (docker *Docker) Run(ctx context.Context,
+	cConfig container.Config,
+	hConfig container.HostConfig,
+	nConfig network.NetworkingConfig,
+	containerName string,
+) error {
+	resp, err := docker.client.ContainerCreate(ctx, &cConfig, &hConfig, &nConfig, nil, containerName)
+	if err != nil {
+		return err
+	}
+
+	if err := docker.client.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RemoveImage removes an image from the host
+func (docker *Docker) RemoveImage(ctx context.Context, imageID string) (string, error) {
+	response, err := docker.client.ImageRemove(ctx, imageID, types.ImageRemoveOptions{Force: true, PruneChildren: true})
+	if err != nil {
+		return "", err
+	}
+
+	byteArr, err := json.Marshal(&response)
+	if err != nil {
+		return "", err
+	}
+	return string(byteArr), nil
 }
 
 // Build builds a Docker image using a tarfile as context
-func (docker *Docker) Build(ctx context.Context, pathToTar string, buildOptions interface{}) (io.ReadCloser, error) {
+func (docker *Docker) Build(ctx context.Context, pathToTar string, buildOptions types.ImageBuildOptions) (io.ReadCloser, error) {
 	dockerBuildContext, err := os.Open(pathToTar)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to open .tar file: %s", err)
 	}
 
-	if buildOptions == nil {
-		buildOptions = types.ImageBuildOptions{}
-	}
-
-	castedOptions, ok := buildOptions.(types.ImageBuildOptions)
-	if !ok {
-		return nil, fmt.Errorf("Expected type types.ImageBuildOptions but got %T instead", buildOptions)
-	}
-
-	buildResponse, err := docker.client.ImageBuild(ctx, dockerBuildContext, castedOptions)
+	buildResponse, err := docker.client.ImageBuild(ctx, dockerBuildContext, buildOptions)
 
 	if err != nil {
 		return nil, err
--- src/container/types.go
+++ src/container/types.go
@@ -3,11 +3,19 @@ package container
 import (
 	"context"
 	"io"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
 )
 
+// Container generic interface for a Container API
 type Container interface {
-	Build(ctx context.Context, pathToTar string, options interface{}) (io.ReadCloser, error)
 	Login(ctx context.Context, username string, password string, registryURL string) (string, error)
+	Build(ctx context.Context, pathToTar string, options interface{}) (io.ReadCloser, error)
+	Stats(ctx context.Context, containerID string) (io.ReadCloser, error)
 	Pull(ctx context.Context, imageName string) (io.ReadCloser, error)
+	Push(ctx context.Context, imageName string) (io.ReadCloser, error)
+	Run(ctx context.Context, cConfig container.Config, hConfig container.HostConfig, nConfig network.NetworkingConfig, containerName string) error
+	RemoveImage(ctx context.Context, imageID string) (string, error)
 	ListImages(ctx context.Context, options interface{}) (string, error)
 }
