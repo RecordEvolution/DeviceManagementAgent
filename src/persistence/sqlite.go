6b4023f047e71ca19107fb576c44e022776a820d
DewitteRuben <ruben.dewitte@proximus.be>
2021-01-24 10:31:13 +0100

    feat: add basic update/create statements

--- src/persistence/sqlite.go
+++ src/persistence/sqlite.go
@@ -4,8 +4,10 @@ import (
 	"database/sql"
 	"io/ioutil"
 	"path/filepath"
+	"reagent/apps"
 	"runtime"
 	"strings"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -31,10 +33,87 @@ func (sqlite *SQLite) Close() error {
 func (sqlite *SQLite) Init() error {
 	_, b, _, _ := runtime.Caller(0)
 	basepath := filepath.Dir(b)
-	return sqlite.ExecuteFromFile(basepath + "/sql/init-script.sql")
+	return sqlite.executeFromFile(basepath + "/sql/init-script.sql")
 }
 
-func (sqlite *SQLite) ExecuteFromFile(filePath string) error {
+func (sqlite *SQLite) UpdateAppState(appName string, appKey int, stage apps.Stage, oldState apps.AppState, newState apps.AppState) error {
+	// First add new entry in history
+	insertAppHistoryStatement := `INSERT INTO AppStateHistory(app_name, app_key, stage, state, timestamp) VALUES (?, ?, ?, ?, ?)`
+	insertStatement, err := sqlite.db.Prepare(insertAppHistoryStatement) // Prepare statement.
+	if err != nil {
+		return err
+	}
+	_, err = insertStatement.Exec(appName, appKey, stage, oldState, time.Now().Format(time.RFC3339))
+	if err != nil {
+		return err
+	}
+
+	// Update current state
+	updateAppStatement := `UPDATE AppStates SET state = ? WHERE app_key = ? AND stage = ?`
+	updateStatement, err := sqlite.db.Prepare(updateAppStatement) // Prepare statement.
+	if err != nil {
+		return err
+	}
+	_, err = updateStatement.Exec(newState, appKey, stage)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sqlite *SQLite) InsertAppState(appName string, appKey, stage apps.Stage, curState apps.AppState) error {
+	insertAppHistoryStatement := `INSERT INTO AppStates(app_name, app_key, stage, state, timestamp) VALUES (?, ?, ?, ?, ?)`
+	insertStatement, err := sqlite.db.Prepare(insertAppHistoryStatement) // Prepare statement.
+	if err != nil {
+		return err
+	}
+	_, err = insertStatement.Exec(appName, appKey, stage, curState, time.Now().Format(time.RFC3339))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sqlite *SQLite) UpdateDeviceState(oldState DeviceState, newState DeviceState, oldInt NetworkInterface, newInt NetworkInterface) error {
+	// Add new entry in history
+	insertAppHistoryStatement := `INSERT INTO DeviceStateHistory(interface_type, device_status, timestamp) VALUES (?, ?, ?)`
+	insertStatement, err := sqlite.db.Prepare(insertAppHistoryStatement) // Prepare statement.
+	if err != nil {
+		return err
+	}
+	_, err = insertStatement.Exec(oldInt, oldState, time.Now().Format(time.RFC3339))
+	if err != nil {
+		return err
+	}
+
+	// Update current state
+	updateAppStatement := `UPDATE DeviceStates SET device_status = ?, interface_type = ?`
+	updateStatement, err := sqlite.db.Prepare(updateAppStatement) // Prepare statement.
+	if err != nil {
+		return err
+	}
+	_, err = updateStatement.Exec(newState, newInt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sqlite *SQLite) InsertDeviceState(state DeviceState, intf NetworkInterface) error {
+	insertAppHistoryStatement := `INSERT INTO DeviceStates(interface_type, device_status, timestamp) VALUES (?, ?, ?)`
+	insertStatement, err := sqlite.db.Prepare(insertAppHistoryStatement) // Prepare statement.
+	if err != nil {
+		return err
+	}
+	_, err = insertStatement.Exec(intf, state, time.Now().Format(time.RFC3339))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sqlite *SQLite) executeFromFile(filePath string) error {
 	file, err := ioutil.ReadFile(filePath)
 
 	if err != nil {
