25dff24ca98755c405f45e01e946876352ec7cbd
DewitteRuben <ruben.dewitte@proximus.be>
2021-02-22 20:55:48 +0100

    refactor: cleanup main

--- src/main/agent.go
+++ src/main/agent.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"context"
-	"os"
-	"os/signal"
 	"reagent/api"
 	"reagent/apps"
 	"reagent/config"
 	"reagent/container"
-	"reagent/filesystem"
 	"reagent/logging"
 	"reagent/messenger"
 	"reagent/persistence"
@@ -18,44 +15,6 @@ import (
 
 	"github.com/rs/zerolog/log"
 )
-
-func main() {
-	cliArgs, err := config.GetCliArguments()
-	if err != nil {
-		log.Fatal().Stack().Err(err).Msg("failed to parse cli arguments")
-	}
-
-	logging.SetupLogger(cliArgs)
-	err = filesystem.InitDirectories(cliArgs)
-	if err != nil {
-		log.Fatal().Stack().Err(err).Msg("failed to init reagent directories")
-	}
-
-	reswarmConfig, err := config.LoadReswarmConfig(cliArgs.ConfigFileLocation)
-	if err != nil {
-		log.Fatal().Stack().Err(err).Msg("failed to load reswarm config file")
-	}
-
-	generalConfig := config.Config{
-		ReswarmConfig:        reswarmConfig,
-		CommandLineArguments: cliArgs,
-	}
-
-	agent := NewAgent(&generalConfig)
-	err = agent.Init()
-	if err != nil {
-		log.Fatal().Stack().Err(err).Msg("failed to init")
-	}
-
-	agent.ListenForDisconnect()
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt)
-	select {
-	case <-sigChan:
-		return
-	}
-}
 
 type Agent struct {
 	Messenger       messenger.Messenger
