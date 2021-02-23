0b4ef1d1b3a4bf744ea425c4069e5493c0298651
DewitteRuben <ruben.dewitte@proximus.be>
2021-02-23 11:57:44 +0100

    fix: undo seperator because cross compile doesn't like it..

--- src/main.go
+++ src/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
 	"reagent/api"
 	"reagent/apps"
 	"reagent/config"
 	"reagent/container"
+	"reagent/filesystem"
 	"reagent/logging"
 	"reagent/messenger"
 	"reagent/persistence"
@@ -15,6 +18,42 @@ import (
 
 	"github.com/rs/zerolog/log"
 )
+
+func main() {
+	cliArgs, err := config.GetCliArguments()
+	if err != nil {
+		log.Fatal().Stack().Err(err).Msg("failed to parse cli arguments")
+	}
+
+	logging.SetupLogger(cliArgs)
+
+	err = filesystem.InitDirectories(cliArgs)
+	if err != nil {
+		log.Fatal().Stack().Err(err).Msg("failed to init reagent directories")
+	}
+
+	reswarmConfig, err := config.LoadReswarmConfig(cliArgs.ConfigFileLocation)
+	if err != nil {
+		log.Fatal().Stack().Err(err).Msg("failed to load reswarm config file")
+	}
+
+	generalConfig := config.New(cliArgs, reswarmConfig)
+
+	agent := NewAgent(&generalConfig)
+	err = agent.Init()
+	if err != nil {
+		log.Fatal().Stack().Err(err).Msg("failed to init")
+	}
+
+	agent.ListenForDisconnect()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	select {
+	case <-sigChan:
+		return
+	}
+}
 
 type Agent struct {
 	Messenger       messenger.Messenger
