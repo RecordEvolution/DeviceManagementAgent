dd1207eaa301c5b29a965e3be139d1401cc93e1b
DewitteRuben <ruben.dewitte@proximus.be>
2021-03-08 09:28:09 +0100

    feat: remove build files whenever we uninstall a dev app

--- src/apps/uninstall_app.go
+++ src/apps/uninstall_app.go
@@ -1,6 +1,7 @@
 package apps
 
 import (
+	"os"
 	"reagent/common"
 )
 
@@ -10,5 +11,17 @@ func (sm *StateMachine) uninstallApp(payload common.TransitionPayload, app *comm
 		return err
 	}
 
-	return sm.setState(app, common.UNINSTALLED)
+	err = sm.setState(app, common.UNINSTALLED)
+	if err != nil {
+		return err
+	}
+
+	if payload.Stage == common.DEV {
+		config := sm.Container.GetConfig()
+		buildDir := config.CommandLineArguments.AppsBuildDir
+		fileName := payload.AppName + "." + config.CommandLineArguments.CompressedBuildExtension
+		os.Remove(buildDir + "/" + fileName) // removes the build zip if it exists
+	}
+
+	return nil
 }
