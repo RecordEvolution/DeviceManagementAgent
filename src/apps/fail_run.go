22ac11268991aa3190d22315a110271c3d7c43f1
DewitteRuben <ruben.dewitte@proximus.be>
2021-03-08 09:10:00 +0100

    fix: remove container when failed on new start

--- src/apps/fail_run.go
+++ src/apps/fail_run.go
@@ -1,7 +1,18 @@
 package apps
 
-import "reagent/common"
+import (
+	"context"
+	"reagent/common"
+)
 
 func (sm *StateMachine) recoverFailToRunningHandler(payload common.TransitionPayload, app *common.App) error {
+	// make sure to remove any existing container to ensure environment variables are set
+	ctx := context.Background()
+	if payload.Stage == common.DEV {
+		sm.Container.RemoveContainerByID(ctx, payload.ContainerName.Dev, map[string]interface{}{"force": true})
+	} else {
+		sm.Container.RemoveContainerByID(ctx, payload.ContainerName.Prod, map[string]interface{}{"force": true})
+	}
+
 	return sm.runApp(payload, app)
 }
