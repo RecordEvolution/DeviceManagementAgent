9c5315dcd8caccab83f98bbab22ea209fe61a33d
DewitteRuben <ruben.dewitte@proximus.be>
2021-04-19 19:52:55 +0200

    chore: decrease cancel timeout

--- src/messenger/wamp.go
+++ src/messenger/wamp.go
@@ -401,7 +401,7 @@ func clientAuthFunc(deviceSecret string) func(c *wamp.Challenge) (string, wamp.D
 
 func (wampSession *WampSession) UpdateRemoteDeviceStatus(status DeviceStatus) error {
 	config := wampSession.GetConfig()
-	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*3)
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second)
 	defer cancelFunc()
 
 	payload := common.Dict{
