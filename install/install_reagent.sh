1a51cc46f6fa6ccec3dde7484998a209a1f44366
Marko Petzold <marko.petzold@record-evolution.de>
2021-06-01 10:00:28 +0200

    add zsh support

--- install/install_reagent.sh
+++ install/install_reagent.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# User can select 
+# User can select
 # OS: ‘darwin’ and ‘linux’ ... Coming soon: 'windows'
-# ARCHITECTURE: ‘amd64’, ‘arm64’ and ‘armv7’ ... Coming soon: ‘x86’ 
+# ARCHITECTURE: ‘amd64’, ‘arm64’ and ‘armv7’ ... Coming soon: ‘x86’
 
 ####### get the download link and create binary folders
 
@@ -12,26 +12,26 @@ os=$(echo $os_ | awk '{print tolower($0)}')
 
 architecture=$(uname -m)
 
-if [[ $architecture == "x86_64" ]];then 
+if [[ $architecture == "x86_64" ]];then
     architecture="amd64"
 fi
 
-if [[ $architecture == "aarch64" ]];then 
+if [[ $architecture == "aarch64" ]];then
     architecture="arm64"
 fi
 
-if [[ $architecture == *"armv7"* ]];then 
+if [[ $architecture == *"armv7"* ]];then
     architecture="armv7"
 fi
 
-echo "=== Welcome to Reagent Install Script.===" 
+echo "=== Welcome to Reagent Install Script.==="
 echo "=== Running for os : ${os} & architecture : ${architecture}."
 
-# Create the binary folder according to the OS 
+# Create the binary folder according to the OS
 
 if [[ $os = "linux" ]]; then
     binary_folder="/usr/bin/reagent"
-    if [ ! -d ${binary_folder} ];then 
+    if [ ! -d ${binary_folder} ];then
         echo "=== ${binary_folder} does not exist!"
         sudo mkdir -p ${binary_folder}
     fi
@@ -40,19 +40,19 @@ fi
 
 if [[ $os = "darwin" ]]; then
     binary_folder="/usr/local/bin/reagent"
-    if [ ! -d ${binary_folder} ];then 
+    if [ ! -d ${binary_folder} ];then
         echo "=== ${binary_folder} does not exist! Creating it."
         sudo mkdir -p ${binary_folder}
     fi
     version=$(curl --fail https://storage.googleapis.com/re-agent/version.txt)
 fi
 
-if [[ $version!="" ]]; then 
-    # Get the Download Link using VERSION, OS and ARCHITECTURE 
-    echo "=== Downloading the Reagent version = $version." 
-    download_from="${link}/${os}/${architecture}/${version}/reagent" 
-    echo "=== Download link : $download_from" 
-else 
+if [[ $version!="" ]]; then
+    # Get the Download Link using VERSION, OS and ARCHITECTURE
+    echo "=== Downloading the Reagent version = $version."
+    download_from="${link}/${os}/${architecture}/${version}/reagent"
+    echo "=== Download link : $download_from"
+else
     echo "=== ERROR! Version not found. Version file is missing. Process is terminated!"
     exit 1
 fi
@@ -62,7 +62,7 @@ if [[ $os = "linux" ]]; then
     echo "=== ----- Downloading binaries -----"
     if [ ! -f ./reagent ]; then
         wget -O reagent "${download_from}"
-    fi 
+    fi
 
     if [ -f ./reagent ]; then
         chmod +x ./reagent
@@ -70,13 +70,13 @@ if [[ $os = "linux" ]]; then
         sleep 1
         new_path="\"${binary_folder}:\$PATH\""
         sudo mv ${PWD}/reagent ${binary_folder}/
-        echo "export PATH=${new_path}" >> "/home/$USER/.bashrc"  
+        echo "export PATH=${new_path}" >> "/home/$USER/.bashrc"
     fi
 fi
 
 # this script is written for MacOS aka Darwin
 if [[ $os = "darwin" ]]; then
-    
+
     if [ ! -f ./reagent ]; then
         echo "=== ----- Downloading binaries -----"
         curl -o ${PWD}/reagent --head --fail "${download_from}"
@@ -88,14 +88,21 @@ if [[ $os = "darwin" ]]; then
         sudo mv ${PWD}/reagent ${binary_folder}/
         echo "=== moved executable to $binary_folder"
         new_path="\"${binary_folder}:\$PATH\""
-         
+
         if grep -q "$new_path" "/Users/${USER}/.bash_profile"; then
-            echo "=== command is already in /Users/${USER}/.bash_profile" 
+            echo "=== command is already in /Users/${USER}/.bash_profile"
         else
-            echo "export PATH=${new_path}" >> "/Users/${USER}/.bash_profile" 
-            echo "=== command added to /Users/${USER}/.bash_profile" 
+            echo "export PATH=${new_path}" >> "/Users/${USER}/.bash_profile"
+            echo "=== command added to /Users/${USER}/.bash_profile"
         fi
-    fi 
+
+        if grep -q "$new_path" "/Users/${USER}/.zshenv"; then
+            echo "=== command is already in /Users/${USER}/.zshenv"
+        else
+            echo "export PATH=${new_path}" >> "/Users/${USER}/.zshenv"
+            echo "=== command added to /Users/${USER}/.zshenv"
+        fi
+    fi
 fi
 
 
