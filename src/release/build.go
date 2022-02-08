075567e9b7f8cd7327b28ee572db3b2f73045a1c
DewitteRuben <rubendewitte1998@gmail.com>
2022-02-08 12:00:19 +0100

    fix: parsing for arm64

--- src/release/build.go
+++ src/release/build.go
@@ -22,9 +22,8 @@ func GetSystemInfo() (string, string, string) {
 		splitArmArch := strings.Split(arch, "v")
 		if len(splitArmArch) == 2 {
 			variant = "v" + splitArmArch[1]
+			arch = "arm"
 		}
-
-		arch = "arm"
 	}
 
 	return runtime.GOOS, arch, variant
