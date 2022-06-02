a93851004884d222ff95d43d3625efa431ab2ff3
DewitteRuben <rubendewitte1998@gmail.com>
2022-06-03 00:39:22 +0200

    chore

--- src/network/nmw_network.go
+++ src/network/nmw_network.go
@@ -477,10 +477,6 @@ func (n NWMNetwork) getEthernetDevices() ([]networkmanager.DeviceWired, error) {
 		}
 	}
 
-	for _, p := range wiredDevices {
-		log.Debug().Msgf("%+v\n", p)
-	}
-
 	return wiredDevices, nil
 }
 
@@ -912,10 +908,7 @@ func (n NWMNetwork) ListEthernetDevices() ([]EthernetDevice, error) {
 
 			ethernetDevice.IPv4AddressData = parsedIPv4AddressDatas
 		} else {
-			connection, err := n.getConnectionByInterfaceName(ethernetDevice.InterfaceName)
-			if err != nil {
-				return nil, err
-			}
+			connection, _ := n.getConnectionByInterfaceName(ethernetDevice.InterfaceName)
 
 			if connection != nil {
 				settings, err := connection.GetSettings()
