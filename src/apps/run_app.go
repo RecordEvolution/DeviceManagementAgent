a87b50faae7e8b68d1b8bde7062f588be27a6f52
DewitteRuben <rubendewitte1998@gmail.com>
2022-06-02 15:07:01 +0200

    chore: use runtime = nvidia

--- src/apps/run_app.go
+++ src/apps/run_app.go
@@ -310,6 +310,7 @@ func (sm *StateMachine) computeContainerConfigs(payload common.TransitionPayload
 
 	if system.HasNvidiaGPU() {
 		log.Debug().Msgf("Detected a NVIDIA GPU, will request NVIDIA Device capabilities...\n")
+		hostConfig.Runtime = "nvidia"
 		hostConfig.DeviceRequests = []container.DeviceRequest{
 			{
 				Driver: "nvidia",
