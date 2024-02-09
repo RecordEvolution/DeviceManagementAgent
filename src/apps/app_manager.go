f857311a90da11f1e8121f642282d43a96926512
DewitteRuben <rubendewitte1998@gmail.com>
2024-02-09 14:14:44 +0100

    fix: defer statelock

--- src/apps/app_manager.go
+++ src/apps/app_manager.go
@@ -47,7 +47,8 @@ func (am *AppManager) syncPortState(payload common.TransitionPayload, app *commo
 	app.StateLock.Lock()
 	curAppState := app.CurrentState
 	requestedState := app.RequestedState
-	app.StateLock.Unlock()
+
+	defer app.StateLock.Unlock()
 
 	// handle app tunnels
 	portRules, err := tunnel.InterfaceToPortForwardRule(payload.Ports)
@@ -120,10 +121,17 @@ func (am *AppManager) RequestAppState(payload common.TransitionPayload) error {
 		am.StateMachine.Filesystem.CancelFileTransfer(payload.ContainerName.Dev)
 	}
 
+	fmt.Println(payload.CancelTransition)
 	if payload.CancelTransition {
 		log.Debug().Msgf("Cancel request was received for %s (%s) (currently: %s)", app.AppName, app.Stage, app.CurrentState)
 		am.StateMachine.CancelTransition(app, payload)
 		return nil
+	}
+
+	err = am.syncPortState(payload, app)
+	if err != nil {
+		log.Error().Err(err).Msgf("failed to sync port state")
+		return err
 	}
 
 	locked := app.SecureTransition() // if the app is not locked, it will lock the app
@@ -149,12 +157,6 @@ func (am *AppManager) RequestAppState(payload common.TransitionPayload) error {
 	}
 
 	payload.RegisteryToken = token
-
-	err = am.syncPortState(payload, app)
-	if err != nil {
-		log.Error().Err(err).Msgf("failed to sync port state")
-		return err
-	}
 
 	errC := am.StateMachine.InitTransition(app, payload)
 	if errC == nil {
